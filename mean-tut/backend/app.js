b21915db4a35e3fe5d96c7b37182c4c60cf558cc
Hoai Nguyen <hoaius7@gmail.com>
2018-09-20 02:49:02 -0500

    36. Fetching Initial Posts

--- mean-tut/backend/app.js
+++ mean-tut/backend/app.js
@@ -2,9 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "123456",
+      title: "First post",
+      content: "This is the first post coming from the server."
+    },
+    {
+      id: "123457",
+      title: "Second post",
+      content: "This is the second post coming from the server."
+    }
+  ];
+
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 app.use((req, res, next) => {
