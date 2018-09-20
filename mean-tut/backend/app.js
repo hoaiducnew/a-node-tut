c43e33b0ae50899402feb8587c5453c8d031738c
Hoai Nguyen <hoaius7@gmail.com>
2018-09-20 09:31:01 -0500

    52. Storing Data in a Database

--- mean-tut/backend/app.js
+++ mean-tut/backend/app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect("mongodb+srv://aviall:1q2w3e4r@cluster0-w7dan.mongodb.net/test?retryWrites=true")
+mongoose.connect("mongodb+srv://aviall:1q2w3e4r@cluster0-w7dan.mongodb.net/mean-tut?retryWrites=true")
   .then(() => {
     console.log('Connected to database!');
   })
@@ -30,7 +30,7 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
