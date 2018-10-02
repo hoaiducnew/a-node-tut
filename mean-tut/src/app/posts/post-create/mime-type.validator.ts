ca1b1c8c93fe00f96931d5d70d63208f0fa3d4b7
Hoai Nguyen <hoaius7@gmail.com>
2018-10-02 15:50:44 -0500

    82. Updating Posts with Images

--- mean-tut/src/app/posts/post-create/mime-type.validator.ts
+++ mean-tut/src/app/posts/post-create/mime-type.validator.ts
@@ -1,10 +1,13 @@
-import {AbstractControl} from '@angular/forms';
-import {Observable, Observer} from 'rxjs';
+import { AbstractControl } from '@angular/forms';
+import { Observable, Observer, of } from 'rxjs';
 
 export const mimeType = (control: AbstractControl): Promise<{[key: string]: any}> | Observable<{[key: string]: any}> => {
   const file = control.value as File;
   const fileReader = new FileReader();
   const frObs = Observable.create((observer: Observer<{[key: string]: any}>) => {
+    if (typeof(control.value) === 'string') {
+      return of(null);
+    }
     fileReader.addEventListener('loadend', () => {
       const arr = new Uint8Array(fileReader.result).subarray(0, 4);
       let header = '';
--- mean-tut/src/app/posts/post-create/post-create.component.ts
+++ mean-tut/src/app/posts/post-create/post-create.component.ts
@@ -41,8 +41,8 @@ export class PostCreateComponent implements OnInit {
         this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
-          this.post = { id: postData._id, title: postData.title, content: postData.content, imagePath: null };
-          this.form.setValue({ title: this.post.title, content: this.post.content });
+          this.post = { id: postData._id, title: postData.title, content: postData.content, imagePath: postData.imagePath };
+          this.form.setValue({ title: this.post.title, content: this.post.content, image: this.post.imagePath });
         });
       } else {
         this.mode = 'create';
@@ -75,7 +75,12 @@ export class PostCreateComponent implements OnInit {
         this.form.value.image
       );
     } else {
-      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+      );
     }
 
     this.form.reset();
