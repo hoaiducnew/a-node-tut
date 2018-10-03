6993b858bae6e713a7f42e0a3219b439c169fa22
Hoai Nguyen <hoaius7@gmail.com>
2018-10-02 23:43:43 -0500

    96. Creating the User Model

--- mean-tut/src/app/app.module.ts
+++ mean-tut/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { LoginComponent } from './auth/login/login.component';
     PostCreateComponent,
     HeaderComponent,
     PostListComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
